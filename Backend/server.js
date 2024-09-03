233366bda03c30b8750118a15feec3bd0d6fbae3
ThatYoungEngineer <taltude49@gmail.com>
2024-09-03 21:59:10 +0500

    deploy backend

--- Backend/server.js
+++ Backend/server.js
@@ -18,9 +18,9 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
 
-app.get('/', (req, res) => {
-    res.json({message: 'api is working'})
-})
+// app.get('/', (req, res) => {
+//     res.json({message: 'api is working'})
+// })
 
 app.use('/api', routes);
 
